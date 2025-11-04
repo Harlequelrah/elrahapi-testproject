10c57c123fe2e5802cdb488a94c6c969e7a7eaae
Harlequelrah <degbovimax19@gmail.com>
2025-11-04 18:04:40 +0000

    debut d'implementation des tests avec "/"

---
+++ testproject/tests/test_should_return_hello.py
@@ -0,0 +1,15 @@
+import sys
+
+print(sys.path)
+from fastapi.testclient import TestClient
+
+from ..testproject.main import app
+
+client = TestClient(app)
+
+
+def test_should_return_hello():
+    response = client.get("/")
+    expected_value = {"message": "hello"}
+    assert response.status_code == 200
+    assert response.json() == expected_value
